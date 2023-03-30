b549dd151460362959edba428e114afd49b06493
rupokraihan <rupok2321@gmail.com>
2023-03-31 05:22:09 +0600

    Container style complete

--- src/Component/Container/Container.jsx
+++ src/Component/Container/Container.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import './Container.css'
+import React from "react";
+import "./Container.css";
 
 const Container = (props) => {
-  const { authorName, blogTitle, blogCoverImage, authorImage, readTime, publishDate } = props.container;
+  const {
+    authorName,
+    blogTitle,
+    blogCoverImage,
+    authorImage,
+    readTime,
+    publishDate,
+  } = props.container;
   return (
     <div>
       <img className="cover-image" src={blogCoverImage} alt="" />
-      <img className="author-image" src={authorImage} alt="" />
-      <h3>{authorName}</h3>
-      <p>{publishDate}</p>
-      <p>{readTime}</p>
+      <div className="container-info d-flex justify-content-between align-items-center ">
+        <div className="d-flex gap-4">
+          <div>
+            <img className="author-image" src={authorImage} alt="" />
+          </div>
+          <div>
+            <h3 className="author-name">{authorName}</h3>
+            <p className="publish-date">{publishDate}</p>
+          </div>
+        </div>
+        <div>
+          <p className="read-time">{readTime}</p>
+        </div>
+      </div>
       <h1>{blogTitle}</h1>
     </div>
   );
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import './Header.css'
-import logo from '../../images/Ellipse 1.png'
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Ellipse 1.png";
 
 const Header = () => {
   return (
-    <div className=" container d-flex justify-content-between mt-4">
-      <div>
-        <h1>Knowledge Cafe</h1>
+    <div className="container">
+      <div className=" container d-flex justify-content-between mt-4">
+        <div>
+          <h1>Knowledge Cafe</h1>
+        </div>
+        <div>
+          <img src={logo} alt="" />
+        </div>
       </div>
-      <div>
-        <img src={logo} alt="" />
-      </div>
+      <hr></hr>
     </div>
   );
 };
