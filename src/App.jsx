506fc61d46de63cedeac19a7520d1d1ba17d3a96
rupokraihan <rupok2321@gmail.com>
2023-03-31 01:28:44 +0600

    header complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './Component/Header';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import logo from '../images/Ellipse 1.png'
+
+const Header = () => {
+  return (
+    <div className=" container d-flex justify-content-between mt-4">
+      <div>
+        <h1>Knowledge Cafe</h1>
+      </div>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
